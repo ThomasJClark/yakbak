589f505babca96ac41f10597db5a2cb89fae3b00
Tom Clark <the.thomas.j.clark@gmail.com>
2015-12-17 19:07:02 -0500

    Better styling

--- public/app.js
+++ public/app.js
@@ -27,33 +27,40 @@ function reload() {
   reloadFunc(function(baks) {
     baksElement.innerHTML = ""
     baks.forEach(function(bak) {
-      var bakElement = document.createElement("tr");
 
-      var bakScore = document.createElement("td");
+      var upvote = document.createElement("span");
+      upvote.className = "mega-octicon octicon-arrow-up vote";
+      upvote.addEventListener("click", function() { yakbak.vote(bak._id, +1, reload); });
+
+      var downvote = document.createElement("span");
+      downvote.className = "mega-octicon octicon-arrow-down vote";
+      downvote.addEventListener("click", function() { yakbak.vote(bak._id, -1, reload); });
+
+      var bakScore = document.createElement("span");
       bakScore.textContent = bak.score;
-      bakElement.appendChild(bakScore);
+
+      var votingContainer = document.createElement("td");
+      votingContainer.className = "voting-container";
+      votingContainer.appendChild(upvote);
+      votingContainer.appendChild(document.createElement("br"));
+      votingContainer.appendChild(bakScore);
+      votingContainer.appendChild(document.createElement("br"));
+      votingContainer.appendChild(downvote);
 
       var bakText = document.createElement("td");
       bakText.textContent = bak.text;
+
+      var bakElement = document.createElement("tr");
+      bakElement.appendChild(votingContainer);
       bakElement.appendChild(bakText);
-
-      var upvote = document.createElement("td");
-      upvote.innerHTML = "<a href='#'>upvote</a>";
-      upvote.addEventListener("click", function() { yakbak.vote(bak._id, +1, reload); });
-      bakElement.appendChild(upvote);
-
-      var downvote = document.createElement("td");
-      downvote.innerHTML = "<a href='#'>downvote</a>";
-      downvote.addEventListener("click", function() { yakbak.vote(bak._id, -1, reload); });
-      bakElement.appendChild(downvote);
 
       // Highlight the upvote or downvote button to indicate if the user has
       // already upvoted or downvoted this post.
       var uid = yakbak.uid();
       if (bak.upvoters.indexOf(uid) != -1) {
-        upvote.style.opacity = 0.5;
+        upvote.style.color = "#ff737f";
       } else if (bak.downvoters.indexOf(uid) != -1) {
-        downvote.style.opacity = 0.5;
+        downvote.style.color = "#72a0fe";
       }
 
       baksElement.appendChild(bakElement);
